2ddc61226f98e06922bf38399ec6dae4c026f260
habybheart1 <akintobiabibat@yahoo.com>
2025-09-02 04:29:10 +0100

    code edited

--- script.js
+++ script.js
@@ -3,14 +3,3 @@ const navLinks = document.querySelector(".mobile-screen-navBar");
 navToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
-// // for journey content
-// const bgToggle = document.getElementById("booking-bg");
-
-// const bookingToggle = document.getElementById("booking-content");
-// bgToggle.addEventListener("mouseenter", () => {
-//   bookingToggle.classList.add("display");
-// });
-// bgToggle.addEventListener("mouseleave", () => {
-//   bookingToggle.classList.remove("display");
-// });
-
